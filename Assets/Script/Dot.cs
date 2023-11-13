50e38640f07caaefc0863131b9d71548685921bc
ColdDragonFire <49942764+ColdDragonFire@users.noreply.github.com>
2023-11-13 13:08:24 +0100

    Added connection-making functionality to Piece. Merging before adapting to UI configuration

--- Assets/Script/Dot.cs
+++ Assets/Script/Dot.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class Dot : MonoBehaviour, IOccupying
 {
     public DotType dotType;
+
+    private bool isConnected;
+
+    //Consider moving Grid Position storage into here.
+
+    public bool IsConnected { get => isConnected; set => isConnected = value; }
 }
--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -1,19 +1,19 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    public Vector2[] posArray;
+    public Vector2[] gridPosArray;
     public Dot[] dotsArray;
     public Vector2 pivotPoint;
 
     public GameObject testPivotPoint;
 
-    private LineRenderer[] connections;
+    public float dotSpacing;
 
-    private bool rotatable=true;
+    private List<LineRenderer> connections;
+
+    private bool rotatable = true;
 
     public bool testRotate;
 
@@ -35,10 +35,60 @@ public class Piece : MonoBehaviour
     }
     public void LoadPiece()
     {
+        //Goes through each dot and measures grid distance to each other dot.
+        // Distance is used to differentiate adjacent and diagonal dot connections. 
         for (int i = 0; i < dotsArray.Length; i++)
         {
-            dotsArray[i].gameObject.transform.localPosition = posArray[i];
+            //
+            dotsArray[i].gameObject.transform.localPosition = gridPosArray[i];
+
+            if (!dotsArray[i].IsConnected)
+            {
+                List<int> diagonalList = new List<int>();
+                bool foundAdjacent = false;
+                for (int j = 0; j < dotsArray.Length; j++)
+                {
+                    if (i == j) continue;
+
+                    float val = Vector2.Distance(gridPosArray[i], gridPosArray[j]);
+                    if (val > 1.3f) continue;
+
+                    else if (val < 1.1f && !foundAdjacent)
+                    {
+                        diagonalList.Add(j);
+
+                    }
+                    else if (val == 1)
+                    {
+                        foundAdjacent = true;
+                        CreateConnection(dotsArray[i].gameObject, gridPosArray[i], gridPosArray[j]);
+                        dotsArray[i].IsConnected = true;
+                        dotsArray[j].IsConnected = true;
+                    }
+                }
+                if (!foundAdjacent)
+                {
+                    foreach (int j in diagonalList)
+                    {
+                        CreateConnection(dotsArray[i].gameObject, gridPosArray[i], gridPosArray[j]);
+                        dotsArray[i].IsConnected = true;
+                        dotsArray[j].IsConnected = true;
+                    }
+                }
+            }
+
         }
+
+    }
+    public void CreateConnection(GameObject gameObject, Vector2 start, Vector2 end)
+    {
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+        connections.Add(lineRenderer);
+    }
+    public void UpdateConnections()
+    {
 
     }
 
@@ -47,19 +97,19 @@ public class Piece : MonoBehaviour
         if (!rotatable)
             return;
 
-        for (int i = 0; i<posArray.Length; i++)
+        for (int i = 0; i < gridPosArray.Length; i++)
         {
-            //Rotation math for vector rotation around a point. (x,y)?(x?,y?)=(?+(x??)cos??(y??)sin?,?+(x??)sin?+(y??)cos?)
-            //(x,y) is the point that is to be rotated. (?,?) is the pivot point of the rotation.
+            //Rotation math for vector rotation around a point. (x,y)->(x',y')=(a+(x-a)cos(t)-(y-?)sin(t),b+(x-a)sin(t)+(y-b)cos(t))
+            //(x,y) is the point that is to be rotated. (a,b) is the pivot point of the rotation.
             //Since we want a 90 degree rotation, the formula effectively becomes: (x,y)?(??(y??),?+(x??))
-            posArray[i] = new Vector2((pivotPoint.x - (posArray[i].y - pivotPoint.y)),
-                (pivotPoint.y + (posArray[i].x - pivotPoint.y)));
+            gridPosArray[i] = new Vector2((pivotPoint.x - (gridPosArray[i].y - pivotPoint.y)),
+                (pivotPoint.y + (gridPosArray[i].x - pivotPoint.y)));
 
-            dotsArray[i].gameObject.transform.localPosition = posArray[i];
+            dotsArray[i].gameObject.transform.localPosition = gridPosArray[i];
         }
     }
 
-    public void Place(Vector2Int coordinates)
+    public void Place(Vector2 coordinates)
     {
 
     }
