24cf07fcaab0851a985e731e8578d0569462a51f
Anders Tornbjerg <anderslykketornbjerg@gmail.com>
2023-11-14 13:19:01 +0100

    Convert Scne gameobjecter to UI elements

--- Assets/Script/Dot.cs
+++ Assets/Script/Dot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dot : MonoBehaviour, IOccupying
 {
@@ -11,4 +12,22 @@ public class Dot : MonoBehaviour, IOccupying
     //Consider moving Grid Position storage into here.
 
     public bool IsConnected { get => isConnected; set => isConnected = value; }
+
+    public void Setup(DotType targetType)
+    {
+        Image renderer = GetComponent<Image>();
+        
+        switch (targetType)
+        {
+            case DotType.Red:
+                renderer.color = Color.red;
+                break;
+            case DotType.Blue:
+                renderer.color = Color.blue;
+                break;
+            case DotType.Yellow:
+                renderer.color = Color.yellow;
+                break;
+        }
+    }
 }
