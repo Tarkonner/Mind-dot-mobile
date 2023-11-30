ec1dee2af6d46c4d4f0607fca8a50081a0310473
ColdDragonFire <49942764+ColdDragonFire@users.noreply.github.com>
2023-11-30 11:32:46 +0100

    Revert "Changed editor folder name"
    This reverts commit 7b43417950bbba3c36c7b073c0a82744bcf4d0d7.

--- Assets/Levels/LevelConverter.cs
+++ Assets/Levels/LevelConverter.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelConverter
+public class LevelConverter : MonoBehaviour
 {
-    private string version;
-    public bool SaveLevel()
+    // Start is called before the first frame update
+    void Start()
     {
-        bool success = true;
-
-        return success;
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
