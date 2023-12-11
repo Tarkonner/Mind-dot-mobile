5f1f8bef7570f8f0a73b29fc0428caffc6388fab
ColdDragonFire <49942764+ColdDragonFire@users.noreply.github.com>
2023-12-11 11:57:37 +0100

    oh bother. tried cleanup in levelconverter

--- Assets/Levels/LevelConverter.cs
+++ Assets/Levels/LevelConverter.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -16,40 +15,40 @@ public static class LevelConverter
         try
         {
         */
-            LevelPiece[] pieces = new LevelPiece[pEs.Count];
-            for (int i = 0; i < pEs.Count; i++)
-            {
-                pieces[i] = new LevelPiece(pEs[i]);
-            }
+        LevelPiece[] pieces = new LevelPiece[pEs.Count];
+        for (int i = 0; i < pEs.Count; i++)
+        {
+            pieces[i] = new LevelPiece(pEs[i]);
+        }
 
-            LevelShapeGoal[] levelShapeGoals = new LevelShapeGoal[sGEs.Count];
-            for (int i = 0; i < sGEs.Count; i++)
-            {
-                List<Vector2Int> positions = sGEs[i].dotDictionary.Keys.ToList();
-                List<DotElement> types = sGEs[i].dotDictionary.Values.ToList();
+        LevelShapeGoal[] levelShapeGoals = new LevelShapeGoal[sGEs.Count];
+        for (int i = 0; i < sGEs.Count; i++)
+        {
+            List<Vector2Int> positions = sGEs[i].dotDictionary.Keys.ToList();
+            List<DotElement> types = sGEs[i].dotDictionary.Values.ToList();
 
-                levelShapeGoals[i] = new LevelShapeGoal(positions, types);
-            }
+            levelShapeGoals[i] = new LevelShapeGoal(positions, types);
+        }
 
-            string name;
-            if (title == null || title == "")
-            {
-                name = "New Level";
-            }
-            else
-            {
-                name = title;
-            }
+        string name;
+        if (title == null || title == "")
+        {
+            name = "New Level";
+        }
+        else
+        {
+            name = title;
+        }
 
-            string uniquePath = AssetDatabase.GenerateUniqueAssetPath($"Assets/Levels/{name}.asset");
-            string uniqueName = uniquePath.Replace("Assets/Levels/", "");
-            uniqueName = uniqueName.Replace(".asset", "");
+        string uniquePath = AssetDatabase.GenerateUniqueAssetPath($"Assets/Levels/{name}.asset");
+        string uniqueName = uniquePath.Replace("Assets/Levels/", "");
+        uniqueName = uniqueName.Replace(".asset", "");
+        //LevelSO levelObject = LevelSO.CreateInstance("LevelSO");
+        LevelSO levelObject = new LevelSO(version, uniqueName, new LevelBoard(board, boardSize), pieces, levelShapeGoals, lPGs);
 
-            LevelSO levelObject = new LevelSO(version, uniqueName, new LevelBoard(board, boardSize), pieces, levelShapeGoals, lPGs);
-
-            AssetDatabase.CreateAsset(levelObject, uniquePath);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
+        AssetDatabase.CreateAsset(levelObject, uniquePath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
 
         /*}
         catch (System.Exception exception)
@@ -57,7 +56,7 @@ public static class LevelConverter
             Debug.LogError(exception.Message);
             return false;
         }*/
-        
+
         return success;
     }
 
