ac9b8719c2042987a6b6f352691b491923dc0eb5
Anders Tornbjerg <anderslykketornbjerg@gmail.com>
2024-05-06 15:34:59 +0200

    Remove onneeded name spaces

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Android.Gradle.Manifest;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
