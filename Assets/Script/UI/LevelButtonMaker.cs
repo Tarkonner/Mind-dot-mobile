fec2b0d182211910d6ecc64da69ada7093c31805
Anders Tornbjerg <anderslykketornbjerg@gmail.com>
2024-07-01 13:51:40 +0200

    Update level select buttons

--- Assets/Script/UI/LevelButtonMaker.cs
+++ Assets/Script/UI/LevelButtonMaker.cs
@@ -16,11 +16,13 @@ public class LevelButtonMaker : MonoBehaviour
     [SerializeField] float spaceBetweenButtons = 100;
     [SerializeField] GameObject levelHolder;
     [Header("Panels")]
+    private int numberOfPanels = 3;
     [SerializeField] GameObject leftArrowButton;
     [SerializeField] GameObject rightArrowButton;
     [SerializeField] float spaceBetweenPanels = 900;
     [SerializeField] float slideAnimationTime = .5f;
     private List<Transform> holders = new List<Transform>();
+    private LevelSelectButton[,] buttonsBank;
     private int panelCount = 0;
     private int maxPanels;
     [Header("Text")]
@@ -28,20 +30,26 @@ public class LevelButtonMaker : MonoBehaviour
     [Header("Color")]
     [SerializeField] ColorBank colorBank;
 
+
     void Start()
     {
-        maxPanels = (int)MathF.Ceiling((float)levelsBank.levels.Length / 30f) - 1;
+        //Arrow buttons
+        leftArrowButton.SetActive(false);
+
+        maxPanels = (int)MathF.Ceiling((float)levelsBank.levels.Length / (horizontalElements * verticalElements)) - 1;
 
         //Level
         int targetLevel = 0;
 
         Vector2 spawnPoint = new Vector2(-spaceBetweenButtons * horizontalElements / 2 + spaceBetweenButtons / 2, spaceBetweenButtons * verticalElements / 2);
 
-        for (int j = 0; j < 3; j++)
+        //How many buttons
+        buttonsBank = new LevelSelectButton[numberOfPanels, horizontalElements * verticalElements];
+
+        for (int j = 0; j < numberOfPanels; j++)
         {
             //Holder
-            int holderCal = horizontalElements * verticalElements * j;
-            GameObject hold = new GameObject($"{holderCal + 1} - {holderCal}");
+            GameObject hold = new GameObject($"{j} panel");
             hold.transform.SetParent(levelHolder.transform);
             hold.transform.localScale = Vector3.one;
             holders.Add(hold.transform);
@@ -59,10 +67,11 @@ public class LevelButtonMaker : MonoBehaviour
                     //Load level when clicket
                     GameObject spawn = Instantiate(levelButtonPrefab, holders[j]);
                     LevelSelectButton lsb = spawn.GetComponent<LevelSelectButton>();
-                    lsb.targetLevel = targetLevel;
+                    lsb.TargetLevel(targetLevel); //Set level and text
                     Button button = spawn.GetComponent<Button>();
-                    button.onClick.AddListener(() => lsb.LoadLevel());
-
+                    button.onClick.AddListener(lsb.LoadLevel);
+                    //Save button to later
+                    buttonsBank[j, y * verticalElements + x] = lsb;
 
                     //Set to next level
                     targetLevel++;
@@ -70,10 +79,6 @@ public class LevelButtonMaker : MonoBehaviour
                     //Placement
                     Vector2 targetTransform = spawnPoint + new Vector2(x * spaceBetweenButtons, y * -spaceBetweenButtons);
                     button.transform.localPosition = targetTransform;
-
-                    //Text
-                    TextMeshProUGUI textElement = button.GetComponentInChildren<TextMeshProUGUI>();
-                    textElement.text = targetLevel.ToString();
 
                     //Give color
                     Image outerImage = spawn.GetComponent<Image>();
@@ -114,13 +119,26 @@ public class LevelButtonMaker : MonoBehaviour
             if (panelCount == maxPanels)
                 return;
 
+            int targetPanel = (panelCount + 2) % numberOfPanels;
+
             //Move
-            foreach(Transform target in holders)
+            foreach (Transform target in holders)
             {
-                target.DOLocalMove(target.localPosition - new Vector3(spaceBetweenPanels, 0, 0), slideAnimationTime);
+                target.DOLocalMove(target.localPosition - new Vector3(spaceBetweenPanels, 0, 0), slideAnimationTime).OnComplete(() =>
+                {
+                    //Move panel to back
+                    holders[targetPanel].transform.localPosition = new Vector2(spaceBetweenPanels, 0);
+                });
             }
 
             panelCount++;
+            //Updata level load
+            if (panelCount > 0)
+            {                
+                Debug.Log("Target panel: " + targetPanel);        
+                UpdatePanelsButton(targetPanel, true);
+            }
+
 
             //Turn arrow off
             if (panelCount == maxPanels)
@@ -134,25 +152,50 @@ public class LevelButtonMaker : MonoBehaviour
             if (panelCount == 0)
                 return;
 
-            //See if moveing to first panel
-
 
             //Move
+            int targetPanel = (panelCount + 1) % numberOfPanels;
             foreach (Transform target in holders)
             {
-                target.DOLocalMove(target.localPosition + new Vector3(spaceBetweenPanels, 0, 0), slideAnimationTime);
-            }
-
+                target.DOLocalMove(target.localPosition + new Vector3(spaceBetweenPanels, 0, 0), slideAnimationTime).OnComplete(() =>
+                {
+                    //Move to front
+                    holders[targetPanel].transform.localPosition = new Vector2(-spaceBetweenPanels, 0);
+                });
+            }         
             panelCount--;
+            
+            //Updata level load
+            UpdatePanelsButton(targetPanel, false);
+
+
 
             //Turn arrow off
-            if(panelCount == 0)
+            if (panelCount == 0)
                 leftArrowButton.SetActive(false);
 
             rightArrowButton.SetActive(true);
         }
 
         //Update text
-        textMesh.text = $"Level {holders[panelCount].name}";
+        int numberOfLevels = panelCount * horizontalElements * verticalElements;
+        textMesh.text = $"Level {numberOfLevels + 1} - {numberOfLevels + numberOfLevels}";
+    }
+
+    void UpdatePanelsButton(int targetPanel, bool right)
+    {
+        for (int y = 0; y < verticalElements; y++)
+        {     
+            for (int x = 0; x < horizontalElements; x++)
+            {
+                int count = y * verticalElements + x;
+
+                int movementModefier = -1;
+                if (right)
+                    movementModefier = 1;
+                int cal = ((panelCount + movementModefier) * horizontalElements * verticalElements) + count;
+                buttonsBank[targetPanel, count].TargetLevel(cal);
+            }
+        }
     }
 }
--- Assets/Script/UI/LevelSelectButton.cs
+++ Assets/Script/UI/LevelSelectButton.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelSelectButton : MonoBehaviour
 {
     public int targetLevel = 0;
+
+    private TextMeshProUGUI levelText;
+
+    private void Awake()
+    {
+        levelText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void TargetLevel(int targetLevel)
+    {
+        levelText.text = (targetLevel + 1).ToString();
+    }
 
     public void LoadLevel()
     {
