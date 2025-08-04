b18007a2480ac32d2c6b9c17528b60a36d522db6
Anders Tornbjerg <anderslykketornbjerg@gmail.com>
2025-08-04 11:04:32 +0200

    Toturial and Tween
    Pulse animation use orgion scale.
    Change how points to button toturial look

--- Assets/Script/TweenAnimation/PulseAnimation.cs
+++ Assets/Script/TweenAnimation/PulseAnimation.cs
@@ -9,13 +9,19 @@ public class PulseAnimation : MonoBehaviour
     [SerializeField] float pulseAnimationTime = 1;
     [SerializeField] float pulseAnimationScale = .2f;
 
-    // Start is called before the first frame update
+    private Vector3 originalScale;
+
     void Start()
     {
-        //Pulse animation
+        originalScale = transform.localScale;
+
+        // Target scales based on original scale
+        Vector3 scaledUp = originalScale * (1 + pulseAnimationScale);
+        Vector3 scaledDown = originalScale;
+
         pulseSequence = DOTween.Sequence();
-        pulseSequence.Append(transform.DOScale(1 + pulseAnimationScale, pulseAnimationTime / 2));
-        pulseSequence.Append(transform.DOScale(1, pulseAnimationTime / 2));
+        pulseSequence.Append(transform.DOScale(scaledUp, pulseAnimationTime / 2));
+        pulseSequence.Append(transform.DOScale(scaledDown, pulseAnimationTime / 2));
         pulseSequence.SetLoops(-1, LoopType.Restart);
         pulseSequence.Play();
     }
